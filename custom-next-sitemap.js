a00abfb6f2fd2370b5718afe570b112a8d8d96ca
arnovanstaden <arnovanstaden@gmail.com>
2024-02-14 10:09:12 +0100

    remove sitemap

--- custom-next-sitemap.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    siteUrl: 'https://engpr.com',
-    generateRobotsTxt: true, // (optional),
-    exclude: ['/404'],
-    changefreq: 'daily',
-    priority: 0.8,
-    sitemapSize: 7000,
-}
