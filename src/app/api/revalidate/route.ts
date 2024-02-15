f2ab401a1a5a1e44f6742a47e0c6feb41ba076aa
Arno van Staden <arnovanstaden@gmail.com>
2024-02-15 08:06:29 +0100

    Upgrade (#9)
    * upgrade dependencies
    
    * move into src
    
    * start app router migration
    
    * migrate other pages
    
    * add fake projects
    
    * fix styling
    
    * fix icons
    
    * fix mobile menu
    
    * fix metadata
    
    * fix js stuff
    
    * fix build issues
    
    * fix build issues
    
    * fix build issues
    
    * Add team
    
    * fix fonts
    
    * fix lightbox & add projects
    
    * fix images
    
    * remove sitemap
    
    * Add static params
    
    * add sitemap
    
    * Add categories
    
    * landing image
    
    * add webhook
    
    * fix build errors
    
    * add home projects
    
    * add services
    
    * update contact form
    
    * update contact form
    
    * update email
    
    * fix responsive
    
    * mobile filter
    
    * fix team images
    
    * fix mobile menu
    
    * add video
    
    * update video styles
    
    * update video
    
    * update video
    
    * update video
    
    * update phrase
    
    * small size adjustments
    
    * small styles
    
    * update breakpoints
    
    * fix border-box issues
    
    * fix filtering
    
    * update dependencies
    
    * update content
    
    * update revalidation paths

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,10 @@
 import { revalidatePath } from 'next/cache'
 
 export async function GET() {
-  revalidatePath('/', 'layout')
+  revalidatePath('/', 'layout');
+  revalidatePath('/', 'page');
+  revalidatePath('/(home)', 'page');
+  revalidatePath('/projects', 'page');
+  revalidatePath('/projects/[slug]', 'page');
   return Response.json({ revalidated: true, now: Date.now() })
 }
